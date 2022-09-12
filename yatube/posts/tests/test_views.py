54f1a4b044ee7c8d634de8543426487da98e5ada
Алексей Горячев <A1.Goryachev@yandex.ru>
2022-09-12 16:40:00 +0300

    Third Review

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -74,19 +74,8 @@ class PostsViewsTests(TestCase):
         super().tearDownClass()
         shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
 
-    def check_index_context(self, response_post, post):
-        """Проверка созданного поста и поста полученного со страницы."""
-        self.assertEqual(response_post.text, post.text,
-                         'поле text не совпадает с заданным')
-        self.assertEqual(response_post.pk, post.pk,
-                         'поле pk не совпадает с заданным  ')
-        self.assertEqual(response_post.group, post.group,
-                         'поле group не совпадает с заданным')
-        self.assertEqual(response_post.author.username, post.author.username,
-                         'поле author не совпадает с заданным')
-
-    def check_post_detail_context(self, response, post):
-        """Проверка созданного поста и ответа со страницы post_detail."""
+    def check_context(self, response, post):
+        """Сверяем пост со страницы с постом в базе."""
         self.assertEqual(response.context.get('post').group_id,
                          post.group.pk,
                          'Ошибка в поле group')
@@ -94,10 +83,14 @@ class PostsViewsTests(TestCase):
                          'Ошибка в поле text')
         self.assertEqual(response.context.get('post').pk, post.pk,
                          'Передан неверный pk')
+        self.assertEqual(response.context.get('post').pub_date, post.pub_date,
+                         'Ошибка в поле pub_date')
         self.assertEqual(
             response.context.get('post').author.username,
             post.author.username,
             'Передан неверный author')
+        self.assertTrue(response.context.get('post').image,
+                        'не выводится изображение на странице поста')
 
     def test_pages_uses_correct_template(self):
         """URL-адрес использует соответствующий шаблон."""
@@ -126,55 +119,30 @@ class PostsViewsTests(TestCase):
         response = (self.authorized_client.
                     get(reverse('posts:post_detail', kwargs={'post_id':
                                                              self.post.pk})))
-        self.check_post_detail_context(response, self.post)
-        self.assertTrue(response.context.get('post').image,
-                        'не выводится изображение на странице поста')
+        self.check_context(response, self.post)
 
     def test_index_show_correct_context(self):
-        """Проверка правильности вывода постов на главную страницу"""
+        """Проверка правильности вывода постов на главную страницу."""
         response = (self.authorized_client.
                     get(reverse('posts:index')))
-        post = response.context['page_obj'][0]
-        self.check_index_context(post, self.post)
-        self.assertTrue(response.context.get('post').image,
-                        'не выводится изображение на главной странице')
+        self.check_context(response, self.post)
 
     def test_group_list_show_correct_context(self):
-        """ Проверка вывода верных постов на странице пользователя"""
+        """Проверка вывода верных постов на странице группы."""
         response = (self.authorized_client.
                     get(reverse('posts:group_posts',
                                 kwargs={'slug': self.group.slug})))
-        response_another = (self.authorized_client.
-                            get(reverse('posts:group_posts',
-                                kwargs={'slug': self.another_group.slug})))
-        first_group = response.context['group']
-        second_group = response_another.context['group']
-        self.assertEqual(first_group.pk, self.post.group.pk,
-                         'пост не выведен на странице своего группы')
-        self.assertNotEqual(second_group.pk, self.post.group.pk,
-                            'пост выведен на странице чужой группы')
-        self.assertTrue(response.context.get('post').image,
-                        'не выводится изображение на странице группы')
+        self.check_context(response, self.post)
 
     def test_profile_show_correct_context(self):
-        """ Проверка вывода верных постов на странице пользователя"""
+        """Проверка вывода верных постов на странице пользователя."""
         response = (self.authorized_client.
                     get(reverse('posts:profile',
                                 kwargs={'username': 'author'})))
-        response_another = (self.authorized_client.
-                            get(reverse('posts:profile',
-                                kwargs={'username': 'another_author'})))
-        first_author = response.context['author']
-        second_author = response_another.context['author']
-        self.assertEqual(first_author.username, self.post.author.username,
-                         'пост не выведен на странице своего автора')
-        self.assertNotEqual(second_author.username, self.post.author.username,
-                            'пост выведен на странице другого автора')
-        self.assertTrue(response.context.get('post').image,
-                        'не выводится изображение на странице профиля')
+        self.check_context(response, self.post)
 
     def test_create_page_correct_context_fields(self):
-        """Проверка вывода верных полей формы для create"""
+        """Проверка вывода верных полей формы для create."""
         response = (self.authorized_client.
                     get(reverse('posts:post_create')))
         form_fields = {
@@ -189,7 +157,7 @@ class PostsViewsTests(TestCase):
                                       f'ошибка при выводе поля {form_field}')
 
     def test_edit_page_correct_context_fields(self):
-        """Проверка вывода верных полей формы для edit_page"""
+        """Проверка вывода верных полей формы для edit_page."""
         response = (self.authorized_client.
                     get(reverse('posts:post_edit',
                                 kwargs={'post_id': self.post.pk})))
@@ -205,7 +173,7 @@ class PostsViewsTests(TestCase):
                                       f'ошибка при выводе поля {form_field}')
 
     def test_edit_page_show_correct_field_values(self):
-        """ Проверка вывода значений в форму edit_page"""
+        """Проверка вывода значений в форму edit_page."""
         response = (self.authorized_client.
                     get(reverse('posts:post_edit', kwargs={'post_id':
                                                            self.post.pk})))
@@ -216,13 +184,24 @@ class PostsViewsTests(TestCase):
             'ошибка при выводе значения group')
 
     def test_comment_exists_base(self):
-        """ Проверка что комментарий имеется в базе"""
-        self.assertEqual(Comment.objects.filter(
-                         text='Тестовый комментарий').get(),
-                         self.comment)
+        """Проверка что комментарий имеется в базе."""
+        form = {'text': 'Test comment',
+                'post': self.post,
+                'author': self.authorized_client}
+        response = self.authorized_client.post(
+            reverse('posts:add_comment', kwargs={'post_id': self.post.pk}),
+            data=form,
+            follow=True)
+        comment_created = response.context.get('post').comments.filter(
+            post=self.post.pk,
+            text='Test comment').get()
+        comment_base = Comment.objects.filter(
+            post=self.post.pk, text='Test comment').get()
+        self.assertEqual(comment_base, comment_created,
+                         'комментарий в базе отличается от созданного')
 
     def test_comment_adds(self):
-        """ Проверка что комментарий передается на страницу post_detail"""
+        """Проверка что комментарий передается на страницу post_detail."""
         response = (self.authorized_client.
                     get(reverse('posts:post_detail', kwargs={'post_id':
                                                              self.post.pk})))
@@ -231,7 +210,7 @@ class PostsViewsTests(TestCase):
                          'Ошибка при передаче комментария в post_detail')
 
     def test_index_cache(self):
-        """ Проверка кэширования"""
+        """Проверка кэширования."""
         cached_post = Post.objects.create(
             author=self.another_author,
             text='Кэшированный тестовый текст поста'
@@ -276,26 +255,26 @@ class PaginatorTests(TestCase):
 
     def test_first_index_page_contains_ten_records(self):
         response = self.authorized_client.get(reverse('posts:index'))
-        """ Проверка что на 1ой странице главной 10 сообщений"""
+        """Проверка что на 1ой странице главной 10 сообщений."""
         self.assertEqual(len(response.context['page_obj']), 10,
                          'Выведено неверное количество постов')
 
     def test_second_index_page_contains_three_records(self):
-        """ Проверка что на 2ой странице главной 3 сообщения"""
+        """Проверка что на 2ой странице главной 3 сообщения."""
         response = self.authorized_client.get(reverse('posts:index')
                                               + '?page=2')
         self.assertEqual(len(response.context['page_obj']), 3,
                          'Выведено неверное количество постов')
 
     def test_first_group_page_contains_ten_records(self):
-        """ Проверка что на 1ой странице группы 10 сообщений"""
+        """Проверка что на 1ой странице группы 10 сообщений."""
         response = self.authorized_client.get(
             reverse('posts:group_posts', kwargs={'slug': 'test-slug'}))
         self.assertEqual(len(response.context['page_obj']), 10,
                          'Выведено неверное количество постов')
 
     def test_second_group_page_contains_three_records(self):
-        """ Проверка что на 2ой странице группы 3 сообщения"""
+        """Проверка что на 2ой странице группы 3 сообщения."""
         response = self.authorized_client.get(
             reverse('posts:group_posts',
                     kwargs={'slug': 'test-slug'}) + '?page=2')
@@ -303,14 +282,14 @@ class PaginatorTests(TestCase):
                          'Выведено неверное количество постов')
 
     def test_first_profile_page_contains_ten_records(self):
-        """ Проверка что на 1ой странице профиля 10 сообщений"""
+        """Проверка что на 1ой странице профиля 10 сообщений."""
         response = self.authorized_client.get(
             reverse('posts:profile', kwargs={'username': 'author'}))
         self.assertEqual(len(response.context['page_obj']), 10,
                          'Выведено неверное количество постов')
 
     def test_second_profile_page_contains_three_records(self):
-        """ Проверка что на 2ой странице профиля 3 сообщения"""
+        """Проверка что на 2ой странице профиля 3 сообщения."""
         response = self.authorized_client.get(
             reverse('posts:profile',
                     kwargs={'username': 'author'}) + '?page=2')
@@ -347,35 +326,56 @@ class FollowTests(TestCase):
             author=cls.author_followed
         )
 
+    def check_context(self, response, post):
+        """Сверяем пост со страницы с новым постом."""
+        response_post = response.context.get('page_obj')[0]
+        self.assertEqual(response_post.group_id,
+                         post.group.pk,
+                         'Ошибка в поле group')
+        self.assertEqual(response_post.text, post.text,
+                         'Ошибка в поле text')
+        self.assertEqual(response_post.pk, post.pk,
+                         'Передан неверный pk')
+        self.assertEqual(response_post.pub_date, post.pub_date,
+                         'Ошибка в поле pub_date')
+        self.assertEqual(
+            response_post.author.username,
+            post.author.username,
+            'Передан неверный author')
+
     def setUp(self):
         self.authorized_user = Client()
         self.authorized_user.force_login(self.current_user)
+        cache.clear()
 
     def test_new_post_shows_follow(self):
-        """ Проверка новый пост появляется ксли есть подписка на автора"""
+        """Проверка новый пост появляется если есть подписка на автора."""
         new_post = Post.objects.create(
             author=self.author_followed,
-            text='Новый пост отслеживаемого автора'
+            text='Новый пост отслеживаемого автора',
+            group_id=1
         )
         response = (self.authorized_user.
                     get(reverse('posts:follow_index')))
-
-        self.assertIn(new_post.text, response.content.decode(),
-                      'Не выведен пост отслеживаемого автора')
+        self.assertIn(new_post, response.context.get('page_obj'),
+                      'Поста отслеживаемого автора нет на странице')
+        self.check_context(response, new_post)
 
     def test_new_post_hide_unfollow(self):
-        """ Новый пост не отображается если нет подписки на автора """
+        """Новый пост не отображается если нет подписки на автора."""
         new_post = Post.objects.create(
             author=self.author_not_followed,
             text='Новый пост не отслеживаемого автора'
         )
         response = (self.authorized_user.
                     get(reverse('posts:follow_index')))
-        self.assertNotIn(new_post.text, response.content.decode(),
+        self.assertTrue(Post.objects.filter(pk=new_post.pk).exists(),
+                        'Пост существует в базе')
+        self.assertNotIn(new_post, response.context.get('page_obj'),
                          'Выведен пост неотслеживаемого автора')
 
     def test_unfollow(self):
-        """ Проверка возможности отписаться"""
+        """Проверка возможности отписаться."""
         self.authorized_user.get(reverse('posts:profile_unfollow',
                                          kwargs={'username':
                                                  self.author_followed}))
@@ -385,7 +385,7 @@ class FollowTests(TestCase):
                          'Не удалось отписаться от автора ')
 
     def test_follow(self):
-        """ Проверка возможности подписаться"""
+        """Проверка возможности подписаться."""
         self.authorized_user.get(reverse('posts:profile_follow',
                                          kwargs={'username':
                                                  self.author_not_followed}))
